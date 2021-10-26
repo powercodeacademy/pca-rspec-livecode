0e84205060d3fa9fd08723b314c43e540040fa91
smkopp92 <smkopp92@gmail.com>
2021-10-26 08:05:20 -0400

    Revert "Add simplecov"
    This reverts commit b8e142030cb135c10aa528505516c2f18cebbf7b.

--- rails-example/spec/rails_helper.rb
+++ rails-example/spec/rails_helper.rb
@@ -1,7 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'simplecov'
-SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -32,7 +30,6 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
